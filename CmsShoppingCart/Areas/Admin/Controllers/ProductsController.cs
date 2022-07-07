147d5c3654a654c0b74a28bc5b808f0224514aed
Temi Ikoba <temiikoba@yahoo.com>
2022-07-07 19:42:55 +0100

    Add project files.

---
+++ CmsShoppingCart/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,86 @@
+﻿using CmsShoppingCart.Infrastructure;
+using CmsShoppingCart.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly CmsShoppingCartContext context;
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public ProductsController(CmsShoppingCartContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            this.context = context;
+            this.webHostEnvironment = webHostEnvironment;
+        }
+
+        // GET /admin/products
+        public async Task<IActionResult> Index(int p = 1)
+        {
+            int pageSize = 6;
+            var products = context.Products.OrderByDescending(x => x.Id).Include(x => x.Category).Skip((p - 1) * pageSize).Take(pageSize);
+
+            return View(await products.ToListAsync());
+        }
+
+        // GET /admin/products/create
+        public IActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(context.Categories.OrderBy(x => x.Sorting), "Id", "Name");
+
+            return View();
+        }
+
+        // POST /admin/products/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            ViewBag.CategoryId = new SelectList(context.Categories.OrderBy(x => x.Sorting), "Id", "Name");
+
+            if (ModelState.IsValid)
+            {
+                product.Slug = product.Name.ToLower().Replace(" ", "-");
+
+                var slug = await context.Products.FirstOrDefaultAsync(x => x.Slug == product.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "The product already exists.");
+                    return View(product);
+                }
+
+                string imageName = "noimage.png";
+                if (product.ImageUpload != null)
+                {
+                    string uploadsDir = Path.Combine(webHostEnvironment.WebRootPath, "media/products");
+                    imageName = Guid.NewGuid().ToString() + "_" + product.ImageUpload.FileName;
+                    string filePath = Path.Combine(uploadsDir, imageName);
+                    FileStream fs = new FileStream(filePath, FileMode.Create);
+                    await product.ImageUpload.CopyToAsync(fs);
+                    fs.Close();
+                }
+
+                product.Image = imageName;
+
+                context.Add(product);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The product has been added!";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+    }
+}
